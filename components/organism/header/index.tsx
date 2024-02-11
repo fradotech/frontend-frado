e5d9076ba954f7d3ead761da06ebda19781203e6
fradotech <fradotech.id@gmail.com>
2024-02-11 10:47:17 +0700

    style: switch to theme dark and light only

--- components/organism/header/index.tsx
+++ components/organism/header/index.tsx
@@ -89,65 +89,6 @@ const Header = () => {
               defaultChecked={theme === 'dark'}
             />
 
-            {/*Multi themes switcher */}
-            <div className="flex-none">
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                  <div className="w-7 rounded-full">
-                    <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      strokeWidth="0"
-                      viewBox="0 0 512 512"
-                      className="w-7 h-7 text-base-content"
-                      height="1em"
-                      width="1em"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M441 336.2l-.06-.05c-9.93-9.18-22.78-11.34-32.16-12.92l-.69-.12c-9.05-1.49-10.48-2.5-14.58-6.17-2.44-2.17-5.35-5.65-5.35-9.94s2.91-7.77 5.34-9.94l30.28-26.87c25.92-22.91 40.2-53.66 40.2-86.59s-14.25-63.68-40.2-86.6c-35.89-31.59-85-49-138.37-49C223.72 48 162 71.37 116 112.11c-43.87 38.77-68 90.71-68 146.24s24.16 107.47 68 146.23c21.75 19.24 47.49 34.18 76.52 44.42a266.17 266.17 0 0086.87 15h1.81c61 0 119.09-20.57 159.39-56.4 9.7-8.56 15.15-20.83 15.34-34.56.21-14.17-5.37-27.95-14.93-36.84zM112 208a32 32 0 1132 32 32 32 0 01-32-32zm40 135a32 32 0 1132-32 32 32 0 01-32 32zm40-199a32 32 0 1132 32 32 32 0 01-32-32zm64 271a48 48 0 1148-48 48 48 0 01-48 48zm72-239a32 32 0 1132-32 32 32 0 01-32 32z"></path>
-                    </svg>
-                  </div>
-                </label>
-                <ul
-                  tabIndex={0}
-                  className="grid dropdown-content p-3 shadow-lg mt-5 bg-base-200 rounded-lg w-52 max-h-80 overflow-x-auto"
-                >
-                  {themes.map((item) => (
-                    <li
-                      data-theme={item}
-                      key={item}
-                      className={`capitalize w-full flex mb-2 rounded-md last-of-type:mb-0 justify-between items-center px-2 py-2 hover:bg-base-300 transition-all duration-300 cursor-pointer`}
-                      onClick={() => {
-                        setTheme(item)
-                      }}
-                    >
-                      <span className="text-base-content flex items-center gap-2">
-                        {hydrationError && theme === item && (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            className="w-3 h-3 text-primary"
-                          >
-                            <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"></path>
-                          </svg>
-                        )}
-                        {item}
-                      </span>
-                      <div className="flex flex-shrink-0 flex-wrap gap-1 h-full">
-                        <div className="bg-primary w-2 rounded"></div>{' '}
-                        <div className="bg-secondary w-2 rounded"></div>{' '}
-                        <div className="bg-accent w-2 rounded"></div>{' '}
-                        <div className="bg-neutral w-2 rounded"></div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-
             {/* Responsive Sidebar Menu */}
             <svg
               onClick={() => setSidebarOpen(!sidebarOpen)}
